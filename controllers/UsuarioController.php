8ce92ebfc223c8bf5a39e1d07b51f32624f7ef0d
anaqu <anaquero1411@gmail.com>
2025-02-24 20:44:45 +0100

    usuario controller arreglado

--- controllers/UsuarioController.php
+++ controllers/UsuarioController.php
@@ -1,21 +1,23 @@
 <?php
 
-namespace ProyectoFinal\Controllers;
+namespace Controllers;
 
-define('HASH_ALGORITHM', PASSWORD_BCRYPT);
+use Models\Usuario;
+use Exception;
 
-class UsuarioController
-{
-    public function mostrarFormularioRegistro()
-    {
-        require_once 'views/usuario/registro.php';
+class UsuarioController{
+    public function mostrarFormularioRegistro(){
+        require_once __DIR__ . 'views/usuario/registro.php';
     }
 
-    public function registrarUsuario()
-    {
-        $nombre = trim($_POST['nombre']);
-        $email = trim($_POST['email']);
-        $password = trim($_POST['password']);
+    public function registrarUsuario(){
+        session_start();
+
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            $nombre = trim($_POST['nombre']);
+            $email = trim($_POST['email']);
+            $password = trim($_POST['password']);
+        }
 
         // Validar datos
         if (empty($nombre) || empty($email) || empty($password)) {
@@ -30,28 +32,22 @@ class UsuarioController
             exit;
         }
 
-        // Encriptar la contraseña
-        $passwordHash = password_hash($password, HASH_ALGORITHM);
-
         try {
-            $conn = new \PDO("mysql:host=" . getenv('DB_HOST') . ";dbname=" . getenv('DB_NAME'), getenv('DB_USER'), getenv('DB_PASS'));
-            $conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-
-            $stmt = $conn->prepare("INSERT INTO usuarios (nombre, email, password) VALUES (:nombre, :email, :password)");
-            $stmt->bindParam(':nombre', $nombre);
-            $stmt->bindParam(':email', $email);
-            $stmt->bindParam(':password', $passwordHash);
-
-            if ($stmt->execute()) {
-                session_start();
-                $_SESSION['success_message'] = "Usuario registrado exitosamente.";
-                header('Location: /success_page.php');
-                exit();
+            $usuario = new Usuario();
+            if ($usuario->registrarUsuario($nombre, $email, $password)) {
+                $_SESSION['success'] = "Usuario registrado correctamente.";
+                header('Location: views/layout/header.php');
+                exit;
             } else {
-                echo "Error al registrar el usuario.";
+                $_SESSION['error'] = "Error al registrar el usuario.";
+                header('Location: /error.php');
+                exit;
             }
-        } catch (\PDOException $e) {
-            echo "Error de conexión: " . $e->getMessage();
+        } catch (Exception $e) {
+            $_SESSION['error'] = "Error: " . $e->getMessage();
         }
+
+        header('Location: /error.php');
+        exit;
     }
 }
