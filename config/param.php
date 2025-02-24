0efdd274af257a8f03ee14ea7034ee39dbd091e8
anaqu <anaquero1411@gmail.com>
2025-02-24 20:07:33 +0100

    config y db

---
+++ config/param.php
@@ -0,0 +1,5 @@
+<?php
+    define ("URL_BASE", "http://localhost/dashboard/proyecto_final_tienda/");
+    define ('controlador_por_defecto', 'ProductoController');
+    define ('accion_por_defecto', 'index');
+?>
