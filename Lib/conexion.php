e451526aa3822eca4be21fc6cdca6b003c193a67
anaqu <anaquero1411@gmail.com>
2025-02-24 17:59:55 +0100

    commit con la estructura de ficheros y conexion

---
+++ Lib/conexion.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Lib;
+use PDO;
+use PDOException;
+
+class Conexion{
+    private $pdo;
+
+    public function __construct(){
+        try{
+            $this->pdo = new PDO('mysql:host=localhost; dbname=proyecto_final_tienda', 'root', '');
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }catch(PDOException $e){
+            echo 'Error al conectar con la base de datos: ' . $e->getMessage();
+        }
+    }
+
+    public function getPdo(){
+        return $this->pdo;
+    }
+}
+
+?>
