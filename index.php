cad5d4ec12c2de678e734fee2838c5bd54765775
anaqu <anaquero1411@gmail.com>
2025-02-25 02:54:41 +0100

    index y registro

--- index.php
+++ index.php
@@ -1,13 +1,74 @@
 <?php
-    require_once 'config/param.php';
-    require_once 'autoload.php';
+
+    ob_start();
+    session_start();
+    require_once __DIR__ . '/config/param.php';
+    require_once __DIR__ . '/autoload.php';
+
+    define('ACTION_DEFAULT', 'index'); // Define la acción predeterminada
+    define('CONTROLLER_DEFAULT', 'HomeController'); // Define el controlador predeterminado
 
     use Controllers\ErrorController;
 
     // Cabecera de la pagina
     require_once __DIR__ . '/views/layout/header.php';
+
+    function show_error(){
+        $error = new ErrorController();
+        $error->index();
+    }
+
+    if(isset($_GET['controller'])){
+        $nombre_controlador = $_GET['controller'] . 'Controller';
+
+    }elseif(!isset($_GET['controller']) && !isset($_GET['action'])){
+        $nombre_controlador = CONTROLLER_DEFAULT;
+    }else{
+        show_error();
+        exit();
+    }
+
+    if(class_exists($nombre_controlador)){
+        $controlador = new $nombre_controlador();
+
+        if(isset($_GET['action']) && method_exists($controlador, $_GET['action'])){
+            $action = $_GET['action'];
+            $controlador->$action();
+        }elseif(!isset($_GET['controller']) && !isset($_GET['action'])){
+            $action_default = ACTION_DEFAULT;
+            $controlador->$action_default();
+        }else{
+            show_error();
+        }
+    }else{
+        show_error();
+    }
+
+require_once __DIR__ . '/views/layout/footer.php'; 
+
+ob_end_flush();
 ?>
 
-<?php 
-require_once __DIR__ . '/views/layout/footer.php'; 
-?>
+<!-- Contenido
+<div id="central">
+    <div class="productos">
+        <img src="assets/img/lanaDelRey.jpg" alt="Vinyl Lana del Rey" class="product-image">
+        <h4>Lana del Rey</h4>
+        <p>50€</p>
+        <a href="carrito">Comprar</a>
+    </div>
+
+    <div class="productos">
+        <img src="assets/img/arianaGrande.jpg" alt="Ariana Grande Vinyl" class="product-image">
+        <h4>Ariana Grande</h4>
+        <p>35€</p>
+        <a href="carrito">Comprar</a>
+    </div>
+
+    <div class="productos">
+        <img src="assets/img/nirvana.jpg" alt="Vinyl Nirvana" class="product-image">
+        <h4>Nirvana</h4>
+        <p>25€</p>
+        <a href="carrito">Comprar</a>
+    </div>
+</div> -->
--- views/layout/footer.php
+++ views/layout/footer.php
@@ -4,33 +4,40 @@
 <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Footer</title>
 </head>
 <body>
 
-<!-- Contenido -->
-<div id="central">
-    <div class="productos">
-        <img src="assets/img/lanaDelRey.jpg" alt="Vinyl Lana del Rey" class="product-image">
-        <h4>Lana del Rey</h4>
-        <p>50€</p>
-        <a href="carrito">Comprar</a>
-    </div>
 
-    <div class="productos">
-        <img src="assets/img/arianaGrande.jpg" alt="Ariana Grande Vinyl" class="product-image">
-        <h4>Ariana Grande</h4>
-        <p>35€</p>
-        <a href="carrito">Comprar</a>
-    </div>
 
-    <div class="productos">
-        <img src="assets/img/nirvana.jpg" alt="Vinyl Nirvana" class="product-image">
-        <h4>Nirvana</h4>
-        <p>25€</p>
-        <a href="carrito">Comprar</a>
+<footer class="bg-light rounded-lg shadow-sm m-4">
+    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div class="sm:flex sm:items-center sm:justify-between">
+            <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                <img src="/assets/img/logo.jpg" class="h-8" alt="Logo" />
+                <span class="self-center text-2xl font-semibold whitespace-nowrap text-dark">Tienda Vinilos</span>
+            </a>
+            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-700 sm:mb-0">
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">About</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <hr class="my-6 border-gray-300 sm:mx-auto lg:my-8" />
+        <span class="block text-sm text-gray-600 sm:text-center">© 2025 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
     </div>
-</div>
+</footer>
+
+
 <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js" defer></script>
 </body>
 </html>
